7467da0369bd2623dcab601da4ff6259b13a8dc0
Brena Amorim <brena.amorim@live.com>
2021-06-22 14:51:57 -0300

    start project

---
+++ SpeakFruit/SpeakFruit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeakFruit
+//
+//  Created by Brena Amorim on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
